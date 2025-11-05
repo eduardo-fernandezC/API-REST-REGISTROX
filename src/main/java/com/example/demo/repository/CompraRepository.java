48419651093bc7b244e8c5e9393322fae15b01b3
Eduardo Fernández <ed.fernandezc@duocuc.cl>
2025-11-05 00:07:02 -0300

    fix: repositorys arreglados

--- src/main/java/com/example/demo/repository/CompraRepository.java
+++ src/main/java/com/example/demo/repository/CompraRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Compra;
-import com.example.demo.model.Usuario;
-
-import java.util.List;
 
 @Repository
 public interface CompraRepository extends JpaRepository<Compra, Long> {
-    List<Compra> findByUsuario(Usuario usuario);
+
 }
--- src/main/java/com/example/demo/repository/ImagenPerfilRepository.java
+++ src/main/java/com/example/demo/repository/ImagenPerfilRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.ImagenPerfil;
-import com.example.demo.model.Usuario;
 
 @Repository
 public interface ImagenPerfilRepository extends JpaRepository<ImagenPerfil, Long> {
-    ImagenPerfil findByUsuario(Usuario usuario);
+    
 }
